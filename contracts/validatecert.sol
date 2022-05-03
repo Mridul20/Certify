0abc1cdff03a0023b53b48ce456c4be536682be4
Mridul20 <mridulmittal20@gmail.com>
2022-05-03 12:15:59 +0530

    sol file

--- contracts/validatecert.sol
+++ contracts/validatecert.sol
@@ -1,67 +1,158 @@
 pragma solidity >=0.7.0 <0.9.0;
 
-contract validatecert{
+contract certman{
 
-    struct cert
-    {
-        string name;
-        string course;
-        uint duration;
-    }
-
-    struct college{
-        string name;
-        uint id;
-        address[] certs;
-    }
-
-
-    mapping(address=>cert) certificates;
-    mapping(address=>bool) college_add;
-    mapping(address=>college) colleges;
-    mapping(address=>bool) certificate_add;
-
-
-    address owner;
-    uint cid = 0;
-    uint gcertid = 0;
+    address admin;
 
     constructor() public {
-        owner=msg.sender;
+        admin=msg.sender;
     }
 
-    event id(uint clgid);
-    event certificateid(address certificate_id);
-
-    function addCollege(address add,string memory name)  public  returns (uint){
-        if(msg.sender != owner)
-            return 0;
-        address[] memory ea;
-        colleges[add]  =  college(name,cid,ea);
-        cid = cid + 1;
-        college_add[add] = true;
-        emit id(cid-1);
-        return cid - 1;
-    }
-    
-    function checkcoll(address col) view public returns (string memory,uint){
-        return (colleges[col].name,colleges[col].id);
+    struct institute {
+        string name;
+        string acr;
+        string link;
+        string[] course;
+        bool del;
     }
 
-    function addcert(address inst,string memory name,string memory course,uint duration,address ceid)   public {
-        if(college_add[inst] == false)
+    mapping(address=>institute) institutes;
+    address[] InstAddressList;
+    uint instcnt = 0;
+
+    function addInstitute(address instad,string memory _name,string memory _acr,string memory _link,string[] memory _course) public{
+        if(msg.sender != admin)
+            return ;
+
+        institutes[instad] = institute(_name,_acr,_link,_course,false);
+        InstAddressList.push(instad);
+        instcnt = instcnt + 1;
+    }
+
+    function viewInstitute(address instad) public view returns (institute memory i)
+    {
+        string[] memory es;
+        institute memory t = institute("invalid","inv","null",es,false);
+
+        if(msg.sender != admin)
+            return t;
+        
+        if(institutes[instad].del == true)
+            return t;
+
+        return (institutes[instad]);
+    }
+
+    function updateInstitute(address instad,string memory _name,string memory _acr,string memory _link,string[] memory _course) public {
+        if(institutes[msg.sender].del == true)
             return;
-        certificates[ceid] = cert(name,course,duration);
-        colleges[inst].certs.push(ceid);
-        certificate_add[ceid] = true;
-        emit certificateid(ceid);
+
+        institutes[instad].name = _name;
+        institutes[instad].acr = _acr;
+        institutes[instad].link = _link;
+
+        delete institutes[instad];
+        for(uint i=0;i<_course.length;i++)
+            institutes[instad].course.push(_course[i]);   
+    } 
+    
+    function deleteInstitute(address instad) public {
+        if(msg.sender != admin)
+            return;
+        institutes[instad].del = true;
+        instcnt = instcnt - 1;
     }
 
-    function viewcert(address certid) view public returns(string memory name,string memory course,uint dur){
-        string memory s = "invalid";
-        if(certificate_add[certid] != true)
-            return (s,s,0);
-        return (certificates[certid].name,certificates[certid].course,certificates[certid].duration);
+    function viewAllInstitutes() public view returns (institute[] memory)  {
+        
+        institute[] memory ans = new institute[](instcnt);
+        // if(msg.sender != admin)
+        //     return ans;
+        uint j = 0;
+        for(uint i=0;i<InstAddressList.length;i++)
+        {
+            if(institutes[InstAddressList[i]].del == false)
+            {
+                ans[j] = institutes[InstAddressList[i]];
+                j = j  +1 ;
+            }
+        }
+        return ans;
     }
-    
-}
+
+    mapping(address=>string) students;
+    address[] StudAddressList;
+    uint studcnt = 0;    
+
+    function addStudent(address studad,string memory _name) public{
+        students[studad] = _name;
+    }
+
+    struct certificate{
+        address stud;
+        address inst;
+        string course;
+        int duration;  
+        bool del; 
+    }   
+
+    mapping(address=>certificate) certificates;
+    address[] certAddressList;
+
+    function issueCertificate(address certad,address instad,address studad,string memory _course,int dur) public
+    {
+        if(institutes[instad].del == true)
+            return;
+        if(institutes[msg.sender].del == true)
+            return;
+        certificates[certad] = certificate(instad,studad,_course,dur,false);
+        certAddressList.push(certad);
+    }
+
+    function revCertificate(address certad) public
+    {
+        if(institutes[msg.sender].del == true)
+            return;
+        if(institutes[certificates[certad].inst].del == true)
+            return;
+
+        certificates[certad].del = true;
+    }
+
+    function viewCertificate(address certad) public view returns (certificate memory){
+        return certificates[certad];
+    }
+    function viewStudCert(address studad) public view returns (uint cnt,certificate[] memory){
+        uint cnt = 0;
+        for(uint i=0;i<certAddressList.length;i++)
+        {
+            if(certificates[certAddressList[i]].del  == false)
+            {
+                if(certificates[certAddressList[i]].stud == studad)
+                {
+                    if(institutes[certificates[certAddressList[i]].inst].del == false)
+                        cnt = cnt + 1;
+                }
+            }
+        }
+
+        certificate[] memory ans = new certificate[](cnt);
+        uint j = 0;
+        for(uint i=0;i<cnt;i++)
+        {
+            if(certificates[certAddressList[i]].del  == false)
+            {
+                if(certificates[certAddressList[i]].stud == studad)
+                {
+                    if(institutes[certificates[certAddressList[i]].inst].del == false)
+                    {
+                            ans[j] = certificates[certAddressList[i]];
+                            j = j + 1;
+                    }
+                }
+            }         
+        }
+        return (cnt,ans);
+    }
+
+}   
